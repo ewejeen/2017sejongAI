43a4cfb01f6b5d63c2ecac08790bd41bcacf41a9
ewejeen <33116805+ewejeen@users.noreply.github.com>
2017-11-09 16:10:53 +0900

    Update Text Tokenizer.py

--- week 9/Text Tokenizer.py
+++ week 9/Text Tokenizer.py
@@ -1,18 +1,13 @@
-#패키지 호출
 from nltk.tokenize import sent_tokenize, word_tokenize, WordPunctTokenizer
 
-#입력테스트 정의
-input_text = "Do you know how tokenization works? It's actually quite interesting! Let's analyze a couple of sentences and figure it out."
+input_text = "The vehicle - carrying 'several' passengers - collided with a lorry driving at slow speed. Nobody was injured in the incident which city officials say was the fault of the human driver of the lorry. The man was subsequently given a ticket by police."
 
-#입력 텍스트를 문장 토큰으로 나눔
 print("\nSentence Tokenizer:")
 print(sent_tokenize(input_text))
 
 
-#입력 텍스트를 단어 토큰으로 나눔
 print("\nWord Tokenizer : ")
 print(word_tokenize(input_text))
 
-#Word punct tokenizer사용해 입력 텍스트를 단어 토큰으로 나눔
 print("\nWord Punct Tokenizer : ")
 print(WordPunctTokenizer().tokenize(input_text))
