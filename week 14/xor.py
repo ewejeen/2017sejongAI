d0bc82fc55530ee88b532b0a49b6d1149ee97705
ewejeen <33116805+ewejeen@users.noreply.github.com>
2017-12-12 17:18:26 +0900

    Update xor.py

--- week 14/xor.py
+++ week 14/xor.py
@@ -26,3 +26,16 @@ cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
 train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
 predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+    for step in range(10001):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
+
+    h, c, a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
